ed79df69c287ce3e5222e196fed0864d1db42bb1
TaHKuX <tahkux@bk.ru>
2026-02-12 01:47:42 +0400

    ad

--- 08-ansible-01-base_02.25/playbook/script.sh
+++ 08-ansible-01-base_02.25/playbook/script.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-pb="site.yml"
-inv="inventoy/prod.yml"
+if docker inspect ubuntu7 >/dev/null 2>&1; then
+    docker start ubuntu7
+else
+    docker run -d --name ubuntu7 ubuntu tail -f /dev/null &&
+    docker exec ubuntu7 apt update >/dev/null &&
+    docker exec ubuntu7 apt install -y python3 >/dev/null
+fi
 
-docker start ubuntu
-docker start centos7
-docker start fedora7
+if docker inspect centos7 >/dev/null 2>&1; then
+    docker start centos7
+else
+    docker run -d --name centos7 centos:7 tail -f /dev/null
+fi
 
-ansible-playbook -i inventory/prod.yml site.yml --ask-vault-pass
+if docker inspect fedora7 >/dev/null 2>&1; then
+    docker start fedora7
+else
+    docker run -d --name fedora7 centos:7 tail -f /dev/null
+fi
 
-docker stop ubuntu
+ansible-playbook -i inventory/prod.yml site.yml \
+  --vault-password-file <(echo netology)
+
+docker stop ubuntu7
 docker stop centos7
 docker stop fedora7
